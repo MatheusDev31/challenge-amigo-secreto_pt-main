0e4c8c782719e0cbeafd05b806249929dd994bb9
Matheus Aparecido Ferreira de Santana <matheus.santana57@outlook.com>
2025-02-06 08:26:31 -0300

    implementando uma função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    if (amigo == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+    }
+
+    limparCampo();  
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
